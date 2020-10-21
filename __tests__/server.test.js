4b97f5abae013d4c10c505a803b3635c11bbb3ea
bayanalalem <bayanalalem0@gmail.com>
2020-10-21 21:12:31 +0300

    done

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -3,35 +3,29 @@ const supertest = require('supertest');
 const mockRequest = supertest(server);
 
 describe('api server products', () => {
-
-
-
-  it('should respond with 200 on a correct route', () => {
-    return mockRequest.get('/api/v1/products').then((results) => {
-      expect(results.status).toBe(200);
-    });
+  it('should respond with 200 on a correct route', async () => {
+    const results = await mockRequest.get('/api/v1/products');
+    expect(results.status).toBe(200);
   });
-  it('should respond with 302 on a wrong method', () => {
-    return mockRequest.get('/api/v1/products/:id').then((results) => {
-      expect(results.status).toBe(302);
-    });
+  it('should respond with 302 on a wrong method', async () => {
+    const results = await mockRequest.get('/api/v1/products/:id');
+    expect(results.status).toBe(302);
   });
   it('should respond with 201 for post', () => {
     return mockRequest.post('/api/v1/products').then((results) => {
       expect(results.status).toBe(201);
     });
-  })
-  it('should respond with 201 for post', async() => {
+  });
+  it('should respond with 201 for post', async () => {
     await mockRequest
-    .put('/api/v1/products/1')
-    .send({ name: 'Joe', category:"m",description:"mm"});
-const res = await mockRequest.get('/api/v1/products/1');
-const response = 
-    { name: 'Joe', id: 1,category:"m",description:"mm"}
+      .put('/api/v1/products/1')
+      .send({ name: 'Joe', category: 'm', description: 'mm' });
+    const res = await mockRequest.get('/api/v1/products/1');
+    const response = { name: 'Joe', id: 1, category: 'm', description: 'mm' };
 
-expect(res.status).toBe(302);
-expect(res.body).toEqual(response);
-})
+    expect(res.status).toBe(302);
+    expect(res.body).toEqual(response);
+  });
   it('should respond with 201 for post', () => {
     return mockRequest.delete('/api/v1/products/:id').then((results) => {
       expect(results.status).toBe(202);
@@ -40,39 +34,39 @@ expect(res.body).toEqual(response);
 });
 
 describe('api server categories', () => {
-
-
-
-    it('should respond with 200 on a correct route', () => {
-      return mockRequest.get('/api/v1/categories').then((results) => {
-        expect(results.status).toBe(200);
-      });
+  it('should respond with 200 on a correct route', () => {
+    return mockRequest.get('/api/v1/categories').then((results) => {
+      expect(results.status).toBe(200);
     });
-    it('should respond with 302 on a wrong method', () => {
-      return mockRequest.get('/api/v1/categories/:id').then((results) => {
-        expect(results.status).toBe(302);
-      });
+  });
+  it('should respond with 302 on a wrong method', () => {
+    return mockRequest.get('/api/v1/categories/:id').then((results) => {
+      expect(results.status).toBe(302);
     });
-    it('should respond with 201 for post', () => {
-      return mockRequest.post('/api/v1/categories').then((results) => {
-        expect(results.status).toBe(201);
-      });
-    })
-    it('should respond with 201 for post', async() => {
-        await mockRequest
-        .put('/api/v1/categories/1')
-        .send({ name: 'Joe', display_name:"m",description:"mm"});
+  });
+  it('should respond with 201 for post', () => {
+    return mockRequest.post('/api/v1/categories').then((results) => {
+      expect(results.status).toBe(201);
+    });
+  });
+  it('should respond with 201 for post', async () => {
+    await mockRequest
+      .put('/api/v1/categories/1')
+      .send({ name: 'Joe', display_name: 'm', description: 'mm' });
     const res = await mockRequest.get('/api/v1/categories/1');
-    const response = 
-        { name: 'Joe', id: 1,display_name:"m",description:"mm"}
-    
+    const response = {
+      name: 'Joe',
+      id: 1,
+      display_name: 'm',
+      description: 'mm',
+    };
+
     expect(res.status).toBe(302);
     expect(res.body).toEqual(response);
-})
-it('should respond with 201 for post', () => {
-  return mockRequest.delete('/api/v1/categories/:id').then((results) => {
-    expect(results.status).toBe(202);
+  });
+  it('should respond with 201 for post', () => {
+    return mockRequest.delete('/api/v1/categories/:id').then((results) => {
+      expect(results.status).toBe(202);
+    });
   });
 });
-    });
-  
